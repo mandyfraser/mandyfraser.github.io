a1cea2b4af5009d4e0837acd0c33108c4a6f6ed5
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-02-14 10:36:28 -0600

    primitive paint

--- MandyPrimitivePaint/sketch.js
+++ MandyPrimitivePaint/sketch.js
@@ -5,8 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let shapeArray = [];
+let squareArray = [];
+let ellipseArray = [];
+let triangleArray = [];
 let shapeSize;
+let shape = " ";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,21 +21,36 @@ function setup() {
 
 function draw() {
   background(255);
-  for (let i = 0; i < shapeArray.length; i++){
-    keyTyped();
+  for (let i = 0; i < squareArray.length; i++){
+    rect(squareArray[i][0], squareArray[i][1], squareArray[i][2], squareArray[i][3]);
   }
+  for (let i = 0; i < ellipseArray.length; i++){
+    ellipse(ellipseArray[i][0], ellipseArray[i][1], ellipseArray[i][2], ellipseArray[i][3]);
+  }
+  for (let i = 0; i < triangleArray.length; i++){
+    triangle(triangleArray[i][0], triangleArray[i][1], triangleArray[i][2], triangleArray[i][3]);
+  }
+  if (shape === "square"){
+    rect(mouseX,mouseY,100,60);
+  }
+  ellipse(mouseX,mouseY,100,100);
+  triangle(mouseX,mouseY,mouseX + 40,mouseY + 80, mouseX + 80, mouseY);
+  //keyTyped();
 }
 
 function keyTyped() {
   let cur = [mouseX, mouseY, shapeSize];
-  shapeArray.push(cur);
   if (key === "a") {
+    shape = "square";
+    squareArray.push(cur);
     rect(mouseX,mouseY,100,60);
   } 
   if (key === "s") {
+    ellipseArray.push(cur);
     ellipse(mouseX,mouseY,100,100);
   }
   if (key === "d") {
+    triangleArray.push(cur);
     triangle(mouseX,mouseY,mouseX + 40,mouseY + 80, mouseX + 80, mouseY);
   }
 }
