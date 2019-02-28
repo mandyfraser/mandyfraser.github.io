888560c7428ea6fe6949e70d49f880d885d92cd6
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-02-28 10:35:15 -0600

    08 loops demo

---
+++ 08 loops demo/sketch.js
@@ -0,0 +1,42 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x = 150;
+let y = 150;
+let colours = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colours.push(color(255,0,0));
+  colours.push(color(0,255,0));
+  colours.push(color(0,0,255));
+  colours.push(color(200,50,5));
+  colours.push(color(5,50,200));
+}
+
+function drawTargetWhile(){
+  let diameter = 200;
+  let counter = 0;
+  while(counter < 5){
+    ellipse(width/2, height/2, diameter, diameter);
+    counter ++;
+    diameter -= 40;
+  }
+}
+
+function drawTargetFor(){
+  for (let i = 5; i > 0; i--){
+    fill(colours[i-1]);
+    ellipse(x,y, i*40, i*40);
+  }
+}
+
+function draw() {
+  background(255);
+  //drawTargetWhile();
+  drawTargetFor();
+}
