25c5bc521d4004fa350a6656cb6d9147104b0746
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-03-01 11:04:05 -0600

    rollover
    aaaaaaaaaaaaaaaaaaa

--- rollover/sketch.js
+++ rollover/sketch.js
@@ -21,6 +21,7 @@ let quad4B = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  pickColour();
 }
 
 function draw() {
@@ -45,13 +46,13 @@ function draw() {
     quad1B = 0;
   }
   else{
-    quad1R += 4;
+    quad1R += random(0,255); //was 4
     quad1G += 4;
     quad1B += 4;
   }
-  quad1R = constrain(quad1R,0,255);
-  quad1G = constrain(quad1G,0,255);
-  quad1B = constrain(quad1B,0,255);
+  // quad1R = constrain(quad1R,0,255);
+  // quad1G = constrain(quad1G,0,255);
+  // quad1B = constrain(quad1B,0,255);
   fill(quad1R,quad1G,quad1B);
   rect(0,0,windowWidth/2,windowHeight/2);
   if (quadrant === 2){
@@ -64,9 +65,9 @@ function draw() {
     quad2G += 4;
     quad2B += 4;
   }
-  quad2R = constrain(quad2R,0,255);
-  quad2G = constrain(quad2G,0,255);
-  quad2B = constrain(quad2B,0,255);
+  // quad2R = constrain(quad2R,0,255);
+  // quad2G = constrain(quad2G,0,255);
+  // quad2B = constrain(quad2B,0,255);
   fill(quad2R,quad2G,quad2B);
   rect(windowWidth/2,0,windowWidth/2,windowHeight/2);
   if (quadrant === 3){
@@ -79,9 +80,9 @@ function draw() {
     quad3G += 4;
     quad3B += 4;
   }
-  quad3R = constrain(quad3R,0,255);
-  quad3G = constrain(quad3G,0,255);
-  quad3B = constrain(quad3B,0,255);
+  // quad3R = constrain(quad3R,0,255);
+  // quad3G = constrain(quad3G,0,255);
+  // quad3B = constrain(quad3B,0,255);
   fill(quad3R,quad3G,quad3B);
   rect(0,windowHeight/2,windowWidth/2,windowHeight/2);
   if (quadrant === 4){
@@ -94,9 +95,27 @@ function draw() {
     quad4G += 4;
     quad4B += 4;
   }
-  quad4R = constrain(quad4R,0,random); //was 255 instead of random, worked fine
-  quad4G = constrain(quad4G,0,random);
-  quad4B = constrain(quad4B,0,random);
+  // quad4R = constrain(quad4R,0,random(0,255)); //was 255 instead of random, worked fine
+  // quad4G = constrain(quad4G,0,random(0,255));
+  // quad4B = constrain(quad4B,0,random(0,255));
   fill(quad4R,quad4G,quad4B);
   rect(windowWidth/2,windowHeight/2,windowWidth/2,windowHeight/2);
+}
+
+function pickColour(){
+  quad1R = constrain(quad1R,0,255);
+  quad1G = constrain(quad1G,0,255);
+  quad1B = constrain(quad1B,0,255);
+
+  quad2R = constrain(quad2R,0,255);
+  quad2G = constrain(quad2G,0,255);
+  quad2B = constrain(quad2B,0,255);
+
+  quad3R = constrain(quad3R,0,255);
+  quad3G = constrain(quad3G,0,255);
+  quad3B = constrain(quad3B,0,255);
+
+  quad4R = constrain(quad4R,0,random(0,255)); //was 255 instead of random, worked fine
+  quad4G = constrain(quad4G,0,random(0,255));
+  quad4B = constrain(quad4B,0,random(0,255));
 }
