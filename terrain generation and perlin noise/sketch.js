83ea14b638645f496346aedbec5c572d378001f8
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-03-14 11:02:13 -0600

    terrain generation and perlin noise
    some progress

--- terrain generation and perlin noise/sketch.js
+++ terrain generation and perlin noise/sketch.js
@@ -6,18 +6,28 @@
 // - describe what you did to take this project "above and beyond"
 
 let rectSize = 5;
+let xoff1 = 0;
+let xoff2 = 10000;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  // let x = map(noise(xoff),0,1,0,width);
+  // xoff += 0.01;
 }
 
 function generateTerrain(){
-  for (let x = 0; x < windowWidth; x += rectSize){
-    // rect(x,height,rectSize,random(-20,-500));
-    rect(x,height,rectSize,noise(0.01));
+  // background(0);
+  for (let i = 0; i < windowWidth; i += rectSize){
+    // fill(0,random(0,255),random(0,255));
+    let x = map(noise(xoff1),0,1,0,width);
+    let y = map(noise(xoff2),0,1,0,height);
+    xoff1 += 0.01;
+    xoff2 += 0.01;
+    rect(x,y,rectSize,random(-20,-500));
   }
 }
 
