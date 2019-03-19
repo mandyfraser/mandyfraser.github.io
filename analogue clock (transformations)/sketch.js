648a9d9a526b8c74f46d12dbce7fc04083cce5db
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-03-19 10:28:18 -0600

    analogue clock (transformations)
    finished

--- analogue clock (transformations)/sketch.js
+++ analogue clock (transformations)/sketch.js
@@ -12,12 +12,41 @@ function setup() {
 
 function draw() {
   background(255);
-  ellipse(width/2,height/2,300);
-
   push();
-  for(let i = 0; i <= 60; i ++){
-    line(width/2, 270, width/2, 280);
-    translate(20,20);
-    rotate(radians(6));
+  translate(width/2,height/2);
+  strokeWeight(9);
+  ellipse(0,0,500,500);
+  for(let i = 0; i < 360; i += 6){
+    push();
+    rotate(radians(i));
+    if (i % 30 === 0){
+      //thick line
+      strokeWeight(5);
+      line(180, 0, 240, 0);
+    }
+    else{
+      //thin line
+      strokeWeight(2);
+      line(200,0,240,0);
+    }
+    pop();
   }
+  //draw clock hands
+  //the seconds hand
+  push();
+  rotate(radians(frameCount));
+  strokeWeight(2);
+  line(0,0,250,0);
+  pop();
+
+  //the minutes hand?
+  push();
+  strokeWeight(4);
+  rotate(radians(frameCount/6));
+  line(0,0,0,-150);
+
+
+  pop();
+
+  pop();
 }
