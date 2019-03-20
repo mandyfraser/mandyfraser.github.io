b621ea308b976fce67d637566e04dfab5a109228
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-03-20 10:25:43 -0600

    13 image animation demo
    finished

--- 13 image animation demo/sketch.js
+++ 13 image animation demo/sketch.js
@@ -6,16 +6,16 @@
 // - describe what you did to take this project "above and beyond"
 
 let lionL, lionR;
-let direction = 1;
-//1 = left
-//2 = right
+let direction = 1;  //1 = left, 2 = right
 let pinImages = [];
+let counter = 0;
+let speed = 1;  //1-8
 
 function preload(){
   lionL = loadImage("assets/lion-left.png");
   lionR = loadImage("assets/lion-right.png");
   for (let i = 0; i < 9; i++){
-    pinImages.push(loadImage("assets/pin-00.png"));
+    pinImages.push(loadImage("assets/pin-0" + i + ".png"));
   }
 }
 
@@ -26,8 +26,14 @@ function setup() {
 function draw() {
   background(255);
   lions();
-  image(pinImages[0],width/2,height/2);
-
+  image(pinImages[counter],width/2,height/2);
+  speed = map(mouseX,0,width,1,8);
+  if (frameCount % int(speed) === 0){    //map returns floats, can't do % division with floats
+    counter ++;
+    if (counter > 8){
+      counter = 0;
+    }
+  }
 }
 
 function lions(){
