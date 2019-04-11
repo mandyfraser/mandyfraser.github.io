81fbdbb92b4d1dd659acea8b2fa357e656ee321c
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-04-11 10:16:22 -0600

    17.5 saving and resolution

---
+++ 17.5 saving and resolution/sketch.js
@@ -0,0 +1,23 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(4500, 3000);
+}
+
+function draw() {
+  background(22);
+  fill(255);
+  ellipse(width/2,height/2,400,400)
+}
+
+function keyPressed(){
+  if(key === " "){
+    save();
+  }
+}
