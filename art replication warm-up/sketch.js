efa5263665312adb43afa86ad32c546ab55b7ba9
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-04-12 11:00:30 -0600

    art replication warm-up
    ...

--- art replication warm-up/sketch.js
+++ art replication warm-up/sketch.js
@@ -5,29 +5,43 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x1;
+let x2;
+let y1 = 0;
+let y2;
+let a = 3;
+let b = 2;
+let c = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x1 = width/2;
   noLoop();
 }
 
 function draw() {
   background(255);
   lines();
+  print(y2);
 }
 
 function lines(){
-  let a = 3;
-  let b = 2;
-  let c = 1;
-  let x1 = width/2;
-  let x2;
   for(let i = 0; i < 100; i ++){
-    x2 = a * (-(((random(0,100) - b)^2) / (2 * (c ^ 2))));
-    line(x1, (a * (x1 ^ 2)) + (b * x1) + c, x2,  (a * x2 ^ 2) + (b * x2) + c);
+    x2 = random(width/3, width - (width/3));
+    getY2();
+    line(x1, y1, x2, y2);
     x1 = x2;
-    a += random(0,100);
-    b += random(0,100);
-    c += random(0,100);
+    y1 = y2;
+    a += random(100,500);
+    b += random(100,500);
+    c += random(100,500);
+  }
+}
+
+function getY2(){
+  // y2 = ((a * (x2 ** 2)) + (b * x2) + c);  //ax^2 + bx + c
+  y2 = random(y1, height);
+  if(y2 >= height){
+    y2 -= height;
   }
 }
