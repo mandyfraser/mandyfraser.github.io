4d0dc9d59b3fc48a28df0756b8101cacdba0be7b
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-04-15 10:44:34 -0600

    art replication warm-up
    finished!  :^)

--- art replication warm-up/sketch.js
+++ art replication warm-up/sketch.js
@@ -1,17 +1,15 @@
-// Project Title
-// Your Name
-// Date
+// Art Replication Warm-Up - Noll's "Gaussian Quadratic"
+// Mandy Fraser
+// 4/15/19
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 let x1;
 let x2;
-let y1 = 0;
+let y1 = 5;
 let y2;
-let a = 3;
-let b = 2;
-let c = 1;
+let yIncrement = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -22,26 +20,24 @@ function setup() {
 function draw() {
   background(255);
   lines();
-  print(y2);
 }
 
 function lines(){
   for(let i = 0; i < 100; i ++){
-    x2 = random(width/3, width - (width/3));
+    x2 = random(width/3, width - (width/3));   //the lines are only drawn in the middle third of the screen
     getY2();
+    if(y2 >= height){              //if a line is going through the bottom of the screen, make it come back onto the screen from the top
+      y2 = height;
+      line(x1, y1, x2, y2);
+      y2 = 0;
+    }
     line(x1, y1, x2, y2);
-    x1 = x2;
-    y1 = y2;
-    a += random(100,500);
-    b += random(100,500);
-    c += random(100,500);
+    x1 = x2;                      //the first y-coordinate of each line is the same as the second y-coordinate of the last line
+    y1 = y2;                      //keeps all the lines connected
   }
 }
 
 function getY2(){
-  // y2 = ((a * (x2 ** 2)) + (b * x2) + c);  //ax^2 + bx + c
-  y2 = random(y1, height);
-  if(y2 >= height){
-    y2 -= height;
-  }
+  y2 = y1 + yIncrement;
+  yIncrement += random(1,3);      //the y-coordinate of each line is 1-3 greater than the last
 }
