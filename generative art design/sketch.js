819a502af74b6cbb80dc447a646b72ad5d4fa834
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-04-16 11:03:50 -0600

    generative art design
    yee yee
    
    time for COLOURS

--- generative art design/sketch.js
+++ generative art design/sketch.js
@@ -1,6 +1,6 @@
 // Generative Art Design
 // Mandy Fraser
-// Date
+// 4/15/19
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -8,21 +8,50 @@
 
 
 function setup() {
-  createCanvas(4500, 3000);
+  createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
 }
 
-function drawThingy(){
+function colours(){
+  let thingyOutR = 0;
+  let thingyOutG = 0;
+  let thingyOutB = 0;
+  let thingyInR = 0;
+  let thingyInG = 0;
+  let thingyInB = 0;
+}
+
+function drawThingyOutside(){
   let xOff = 1;
-  for(let x = 0; x <= width; x++){
-    rect(x, noise(xOff) * height, x + 1, height);
-    xOff += 0.01;
+  let squareWidth = 0.1;
+  for(let x = 0 - (width/2); x < width/2; x++){          //was (let x = 0 - (width/2); x <= width/2; x++), then (let x = 0 - 360; x < 360; x++)
+    push();
+    rotate(radians(x*2));  //rotate by x or x*2?
+    rect(x, noise(xOff) * -height/2, x + squareWidth, 0);
+    pop();
+    xOff += 0.025;
+  }
+}
+
+function drawThingyInside(){
+  let xOff = 1;
+  let squareWidth = 0.1;
+  for(let x = 0 - (width/2); x < width/2; x++){          //was (let x = 0 - (width/2); x <= width/2; x++), then (let x = 0 - 360; x < 360; x++)
+    push();
+    rotate(radians(x*2));  //rotate by x or x*2?
+    rect(x, noise(xOff) * height/2, x + squareWidth, 0);
+    pop();
+    xOff += 0.025;
   }
 }
 
 function draw() {
   background(255);
-  drawThingy();
+  push();
+  translate(width/2, height/2);
+  drawThingyOutside();
+  drawThingyInside();
+  pop();
 }
 
 function keyPressed(){
