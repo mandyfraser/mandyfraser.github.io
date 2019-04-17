a270abd60cd52093be6112e74e9b22076b9b2cfd
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-04-17 11:02:52 -0600

    generative art design
    COLOURS

--- generative art design/sketch.js
+++ generative art design/sketch.js
@@ -5,26 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let thingyOutR = 0;
-let thingyOutG = 0;
-let thingyOutB = 0;
-let thingyInR = 0;
-let thingyInG = 0;
-let thingyInB = 0;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
-}
-
-function colours(){
-
+  noLoop();
 }
 
 function drawThingyOutside(){
   let xOff = 1;
   let squareWidth = 0.1;
+  let thingyHue;
   for(let x = 0 - (width/2); x < width/2; x++){          //was (let x = 0 - (width/2); x <= width/2; x++), then (let x = 0 - 360; x < 360; x++)
+    thingyHue = constrain(x, 100, 275);
+    if(thingyHue % 50 === 0){
+      stroke(thingyHue % 360,280,330);
+    }
     push();
     rotate(radians(x*2));  //rotate by x or x*2?
     rect(x, noise(xOff) * -height/2, x + squareWidth, 0);
@@ -36,7 +31,12 @@ function drawThingyOutside(){
 function drawThingyInside(){
   let xOff = 1;
   let squareWidth = 0.1;
+  let thingyHue;
   for(let x = 0 - (width/2); x < width/2; x++){          //was (let x = 0 - (width/2); x <= width/2; x++), then (let x = 0 - 360; x < 360; x++)
+    thingyHue = constrain(x, 100, 275);
+    if(thingyHue % 50 === 0){
+      stroke(-x % 360,280,330);
+    }
     push();
     rotate(radians(x*2));  //rotate by x or x*2?
     rect(x, noise(xOff) * height/2, x + squareWidth, 0);
@@ -46,7 +46,9 @@ function drawThingyInside(){
 }
 
 function draw() {
+  colorMode(RGB);
   background(255);
+  colorMode(HSB,360);
   push();
   translate(width/2, height/2);
   drawThingyOutside();
