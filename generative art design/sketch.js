6ea22e9168d74dcc884b4134bc617b7d0dc08e4c
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-05-03 10:24:44 -0600

    generative art design
    finished  :^)

--- generative art design/sketch.js
+++ generative art design/sketch.js
@@ -14,13 +14,13 @@ function setup() {
 function drawSpiralOutside(){
   let xOff = 1;
   let squareWidth = 0.1;
-  for(let x = 0 - (width/2); x < width/2; x++){          //was (let x = 0 - (width/2); x <= width/2; x++), then (let x = 0 - 360; x < 360; x++)
+  for(let x = 0 - (width/2); x < width/2; x++){          
     if(x % 50 === 0){
       // stroke(205,x,330);
       stroke(205,map(x,-width /2, width/2, 0, 255),330);
     }
     push();
-    rotate(radians(x));  //rotate by x or x*2?
+    rotate(radians(x/2));  //rotate by x or x*2?
     rect(x, noise(xOff) * -height/2, x + squareWidth, 0);
     pop();
     xOff += 0.025;
@@ -30,12 +30,12 @@ function drawSpiralOutside(){
 function drawSpiralInside(){
   let xOff = 1;
   let squareWidth = 0.1;
-  for(let x = 0 - (width/2); x < width/2; x++){          //was (let x = 0 - (width/2); x <= width/2; x++), then (let x = 0 - 360; x < 360; x++)
+  for(let x = 0 - (width/2); x < width/2; x++){          
     if(x % 50 === 0){
       stroke(205,map(x,-width /2, width/2, 0, 255),330);
     }
     push();
-    rotate(radians(x));  //rotate by x or x*2?
+    rotate(radians(x/2));  //rotate by x or x*2?
     rect(x, noise(xOff) * height/2, x + squareWidth, 0);
     pop();
     xOff += 0.025;
@@ -46,7 +46,6 @@ function draw() {
   colorMode(RGB);
   background(0);
   colorMode(HSB,360);
-  // stroke(255);
   push();
   translate(width/2, height/2);
   drawSpiralOutside();
