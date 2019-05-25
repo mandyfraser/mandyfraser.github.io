b39542e89d5659ef7dea12045abc19bc75f34f0c
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-05-24 22:31:42 -0600

    experimenting with sound

--- experimenting with sound/sketch.js
+++ experimenting with sound/sketch.js
@@ -5,16 +5,19 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let sound, fft;
+let sound, fft, soundColourR, soundColourG, soundColourB;
+let bandWidth;
 
 function preload(){
-  sound = loadSound("assets/energy.mp3");
+  sound = loadSound("assets/12 Bloodflood.m4a");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fft = new p5.FFT();
-  sound.setVolume(0.1);
+  bandWidth = width/64;
+  fft = new p5.FFT(0.9,64);
+  sound.setVolume(1);
+  colorMode(RGB);
 }
 
 function mousePressed(){
@@ -23,24 +26,26 @@ function mousePressed(){
 }
 
 function draw() {
-  background(255);
+  background(200);
+  createSpectrum();
+  print(getEnergy("treble"));
+}
+
+function createSpectrum(){
   let spectrum = fft.analyze();
-  fill(0);
   noStroke();
   for(let i = 0; i < spectrum.length; i++){
-    let x = map(i,0,spectrum.length,0,width);
+    let x = map(i,0,64,0,width);
     let y = -height + map(spectrum[i],0,255,height,0);
-    rect(x,height,width/spectrum.length,y);
+    getEnergy("treble");
+    fill(i+5,i+25,i*2);
+    if(i % 1 === 0){
+      rect(x,height,bandWidth - 2,y);
+    }
+    // rect(x,height,width/spectrum.length,y);
   }
 }
 
-// function createSpectrum(){
-//   let spectrum = fft.analyze();
-//   fill(0);
-//   noStroke();
-//   for(let i = 0; i < spectrum.length; i++){
-//     let x = map(i,0,spectrum.length,0,width);
-//     let y = -height + map(spectrum[i],0,255,height,0);
-//     rect(x,height,width/spectrum.length,y);
-//   }
-// }
+function getEnergy(){
+
+}
