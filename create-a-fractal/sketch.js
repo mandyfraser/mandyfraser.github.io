bbcd26cdea272c36e8d1fa95a9b1ae979ceaeceb
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-05-27 11:02:32 -0600

    create-a-fractal

--- create-a-fractal/sketch.js
+++ create-a-fractal/sketch.js
@@ -1,40 +1,45 @@
-// Project Title
-// Your Name
-// Date
+// Create-a-fractal
+// Mandy Fraser
+// 5/27/19
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 let torusDiameter = 50;
-let angle = 5;
-let x = 0;
+let x;
+let c = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  x = 0 - width/2;
   smooth();
-  noLoop();
 }
 
 function draw() {
   background(220);
-  for(let i = 0 - width/2; i < width/2; i += torusDiameter/2){
-    x = i;
-    push();
-    //rotateX(radians(180));
-    // translate(x,0);
-    toruses(x,90);
-    pop();
+  for(let i = 0 - height/2; i < height/2; i += torusDiameter){
+    rotateY(radians(frameCount/2));
+    rotateX(radians(frameCount/2));
+    fill(20,50,200);
+    toruses(x,i,90);
   }
 }
 
-function toruses(xPosition,angle){
-  if(xPosition === 0 - width/2 && xPosition < width/2){
+function toruses(xPosition,yPosition,angle){
+  print(xPosition);
+  if(xPosition > width/2){
+    push();
     rotateX(radians(angle));
-    translate(xPosition,0);
+    translate(xPosition,yPosition);
     torus(torusDiameter,10);
-    toruses(x+torusDiameter*2,angle+90);          //was toruses(x+angle+90,180);
+    pop();
   }
   else{
-    // toruses(x+torusDiameter*2,angle+90);
+    push();
+    rotateX(radians(angle));
+    translate(xPosition, yPosition);
+    torus(torusDiameter,10);
+    pop();
+    toruses(xPosition+torusDiameter*2,0,angle+90);
   }
 }
