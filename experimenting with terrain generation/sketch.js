9f22c27ca0d18d902126312f6d58940bb6c9971e
mandyfraser <47329928+mandyfraser@users.noreply.github.com>
2019-06-04 11:05:53 -0600

    experimenting with terrain generation

--- experimenting with terrain generation/sketch.js
+++ experimenting with terrain generation/sketch.js
@@ -5,13 +5,15 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let rectSize = 1;
+let rectSize = 10;
 let start = 0;
 let biker;
 let currentHeight;
 let x,y;
 let b;
 let rects = [];
+let rectX,rectY;
+let wheelDiameter = 30;
 
 function preload(){
   biker = loadImage("assets/spr_bike2man_0.png");
@@ -19,80 +21,104 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bw = new BackWheel(0,0);
-  fw = new FrontWheel(0+50,0);
+  bw = new Wheel(0,0);
+  fw = new Wheel(0+50,0);
 }
 
 function generateTerrain(){
   let xOff = start;
   for(let i = 0; i < width; i+= rectSize){
-    x = i;
+    rectX = i;
     currentHeight = noise(xOff) * height;
-    let y = -height + map(noise(xOff),0,1,height,0);
-    fill(x+5,x+25,x*2);
-    rect(x,height,rectSize,y);
+    rectY = -height + map(noise(xOff),0,1,height,0);
+    // fill(x+5,x+25,x*2);
+    rect(rectX,height,rectSize,rectY);
+    fw.collision(rectX,height,rectSize,rectY);
     // push();
     // translate(x,y*-1);
     // pop();
     xOff += 0.001;
   }
-  start += 0.004;
+  // start += 0.004;
 }
 
 function draw() {
   background(255);
   generateTerrain();
-  bw.move();
-  bw.display();
+  // bw.move();
+  // bw.display();
   fw.move();
   fw.display();
 }
 
-class BackWheel{
+class Wheel{
   constructor(x_,y_){
     this.x = x_;
     this.y = y_;
     this.xSpeed = 10;
     this.ySpeed = 0;
-    this.GRAV = -0.02;
+    this.GRAV = 0.02;
     this.hit = false;
   }
 
+  collision(x,y,w,h){
+    this.hit = collideRectCircle(x,y,w,h,this.x,this.y,wheelDiameter);
+    if(frameCount % 6 === 0){
+      print(x,y,w,h,this.x,this.y,wheelDiameter);
+    }
+    if(this.hit){
+      print(this.hit);
+    }
+    //print(this.hit);
+  }
+
   move(){
-    if(this.hit === false){
+    if(this.hit){
+      this.ySpeed = 0;
+    }
+    else{
       this.ySpeed += this.GRAV;
     }
     this.y += this.ySpeed;
+    
   }
 
   display(){
     ellipseMode(CENTER);
-    ellipse(this.x, this.y, 20);
+    ellipse(this.x, this.y, wheelDiameter);
   }
 }
 
-class FrontWheel{
-  constructor(x_,y_){
-    this.x = x_;
-    this.y = y_;
-    this.xSpeed = 10;
-    this.ySpeed = 0;
-    this.GRAV = -0.02;
-    this.hit = false;
-  }
+// class FrontWheel{
+//   constructor(x_,y_){
+//     this.x = x_;
+//     this.y = y_;
+//     this.xSpeed = 10;
+//     this.ySpeed = 0;
+//     this.GRAV = 0.02;
+//     this.hit = false;
+//   }
 
-  move(){
-    if(this.hit === false){
-      this.ySpeed += this.GRAV;
-    }
-    this.y += this.ySpeed;
-  }
+//   collision(){
+//     this.hit = collideRectCircle(rectX,height,rectSize,rectY,this.x,this.y,wheelDiameter);
+//   }
 
-  display(){
-    ellipseMode(CENTER);
-    ellipse(this.x, this.y, 20);
-  }
-}
+//   move(){
+//     this.collision();
+//     if(this.hit === false){
+//       this.ySpeed += this.GRAV;
+//     }
+//     else{
+//       this.ySpeed = 0;
+//     }
+//     this.y += this.ySpeed;
+//   }
+
+//   display(){
+//     ellipseMode(CENTER);
+//     ellipse(this.x, this.y, wheelDiameter);
+//   }
+// }
 
 // class Biker{
 //   constructor(x1_,y1_,x2_,y2_){
